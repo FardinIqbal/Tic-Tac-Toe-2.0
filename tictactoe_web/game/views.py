37906fc123bbd512e06e220a9fdeef337ca40477
LemonTea333 <52869603+FardinIqbal@users.noreply.github.com>
2024-07-30 23:43:35 -0400

    Update views to use TicTacToe game logic

--- tictactoe_web/game/views.py
+++ tictactoe_web/game/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .game_logic import TicTacToe
 
-# Create your views here.
+game_instance = TicTacToe()
+
+def index(request):
+    context = {'board': game_instance.board}
+    return render(request, 'game/index.html', context)
+
+def make_move(request):
+    row = int(request.POST.get('row'))
+    col = int(request.POST.get('col'))
+    message = game_instance.get_move(row, col)
+    context = {
+        'board': game_instance.board,
+        'message': message
+    }
+    return render(request, 'game/index.html', context)
